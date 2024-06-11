2eb17e9a4f3498df802b7dced3e3fd7843e6898c
dragneel30 <lorencehernandez97@gmail.com>
2024-06-11 20:53:34 +0800

    feat: -added mongodb authentication

--- app/database/config.py
+++ app/database/config.py
@@ -3,9 +3,13 @@ import os
 from pymongo import MongoClient
 
 MONGO_PORT = os.getenv('MONGO_PORT')
-MONGO_HOST = os.getenv('MONGO_HOST')
+MONGO_HOST_PY = os.getenv('MONGO_HOST_PY')
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PASS = os.getenv('MONGO_PASS')
 
-client = MongoClient(MONGO_HOST, int(MONGO_PORT))
+host = 'mongodb://' + MONGO_USER + ':' + MONGO_PASS + '@' + MONGO_HOST_PY + ':' + MONGO_PORT + '/?authSource=pos'
+
+client = MongoClient(host)                
 db = client.pos
 users = db.users
 products = db.products
