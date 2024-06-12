ecbf90fc4a5b964212ee172028980d54df277a44
dragneel30 <lorencehernandez97@gmail.com>
2024-06-12 17:13:31 +0800

    fix: -doctors collection config

--- app/database/config.py
+++ app/database/config.py
@@ -6,10 +6,15 @@ MONGO_PORT = os.getenv('MONGO_PORT')
 MONGO_HOST_PY = os.getenv('MONGO_HOST_PY')
 MONGO_USER = os.getenv('MONGO_USER')
 MONGO_PASS = os.getenv('MONGO_PASS')
+LOCAL = os.getenv('LOCAL')
 
-host = 'mongodb://' + MONGO_USER + ':' + MONGO_PASS + '@' + MONGO_HOST_PY + ':' + MONGO_PORT + '/?authSource=pos'
+if LOCAL: 
+ client = client = MongoClient(MONGO_HOST_PY, int(MONGO_PORT)) 
+else:  
+  host = 'mongodb://' + MONGO_USER + ':' + MONGO_PASS + '@' + MONGO_HOST_PY + ':' + MONGO_PORT + '/?authSource=pos'
+  client = MongoClient(host)   
 
-client = MongoClient(host)                
 db = client.pos
 users = db.users
 products = db.products
+doctors = db.doctors
