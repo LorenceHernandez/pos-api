2faa7a85e8fd557f8f529282b0eb7bf73e166a65
dragneel30 <lorencehernandez97@gmail.com>
2024-07-22 12:33:43 +0800

    feat: - added requestBy and referredBy doctor info

--- app/routes/transaction/read.py
+++ app/routes/transaction/read.py
@@ -45,7 +45,6 @@ def _get_transactions():
          try: 
             branch = branches.find_one({"_id": ObjectId(transaction["branchId"])})
             if branch:
-              print('testset')
               user_branch = {
                     "_id": str(branch["_id"]),
                     "name": branch["name"],
@@ -62,50 +61,51 @@ def _get_transactions():
               print('testset')
               user_branch = None
 
-
-     #     referred_by = None
-     #     requested_by = None
-     #     try: 
-     #           doctor = doctors.find_one({"_id": ObjectId(record["referred_by"])})
-     #           if doctor:
-     #                referred_by = {
-     #                     "_id": str(doctor["_id"]),
-     #                     "firstName": doctor["firstName"],
-     #                     "middleName": doctor["middleName"],
-     #                     "lastName": doctor["lastName"],
-     #                     "age": doctor["age"],
-     #                     "gender": doctor["gender"],
-     #                     "address": doctor["address"],
-     #                     "isMember": doctor["isMember"],
-     #                     "created_by": doctor["created_by"],
-     #                     "created_at": doctor["created_at"]
-     #                }
-     #     except Exception as e: 
-     #                referred_by = None
-     #     try: 
-     #                doctor = doctors.find_one({"_id": ObjectId(record["requested_by"])})
-     #                if doctor:
-     #                     requested_by = {
-     #                          "_id": str(doctor["_id"]),
-     #                          "firstName": doctor["firstName"],
-     #                          "middleName": doctor["middleName"],
-     #                          "lastName": doctor["lastName"],
-     #                          "age": doctor["age"],
-     #                          "gender": doctor["gender"],
-     #                          "address": doctor["address"],
-     #                          "isMember": doctor["isMember"],
-     #                          "created_by": doctor["created_by"],
-     #                          "created_at": doctor["created_at"]
-     #                     }
-     #     except Exception as e: 
-     #                requested_by = None
+         referred_by = None
+         requested_by = None
+         try: 
+                         doctor = doctors.find_one({"_id": ObjectId(transaction["referredBy"])})
+                         if doctor:
+                              referred_by = {
+                                   "_id": str(doctor["_id"]),
+                                   "firstName": doctor["firstName"],
+                                   "middleName": doctor["middleName"],
+                                   "lastName": doctor["lastName"],
+                                   "age": doctor["age"],
+                                   "gender": doctor["gender"],
+                                   "address": doctor["address"],
+                                   "isMember": doctor["isMember"],
+                                   "created_by": doctor["created_by"],
+                                   "created_at": doctor["created_at"]
+                              }
+         except Exception as e: 
+                              referred_by = None
+         try: 
+                              doctor = doctors.find_one({"_id": ObjectId(transaction["requestedBy"])})
+                              if doctor:
+                                   requested_by = {
+                                        "_id": str(doctor["_id"]),
+                                        "firstName": doctor["firstName"],
+                                        "middleName": doctor["middleName"],
+                                        "lastName": doctor["lastName"],
+                                        "age": doctor["age"],
+                                        "gender": doctor["gender"],
+                                        "address": doctor["address"],
+                                        "isMember": doctor["isMember"],
+                                        "created_by": doctor["created_by"],
+                                        "created_at": doctor["created_at"]
+                                   }
+         except Exception as e: 
+                              requested_by = None
      
          new_transaction = merge(
-             omit(transaction, '_id', 'customerId', 'branchId'),
+             omit(transaction, '_id', 'customerId', 'branchId', 'requestedBy', 'referredBy'),
              { 
                "id": str(transaction['_id']),
                "customer": transaction_customer,
                "branch": user_branch,
+               "requestedBy": requested_by,
+               "referredBy": referred_by,
              },
           )
          transaction_list.append(new_transaction)
