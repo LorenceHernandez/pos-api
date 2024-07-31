518168de542a2838bd4a2819eb0b7abf46e76dce
dragneel30 <lorencehernandez97@gmail.com>
2024-08-01 00:42:05 +0800

    fix: - added condition for corporate types for reports

--- app/routes/sales/report_generators/typesOfClient.py
+++ app/routes/sales/report_generators/typesOfClient.py
@@ -47,7 +47,7 @@ def typesOfClient(args):
         for branch in _branches:
            if branch['id'] == transaction['branchId']:
               for type in branch['types']:
-                 if type['name'] == transaction['customerData']['customerType']:
+                 if type['name'] == transaction['customerData']['customerType'] or type['name'] == transaction['customerData']['type']:
                     type['count'] += 1
                     type['amount'] += transaction['paymentDetails']['paymentDue']
                     index = len(branch['types']) - 1
