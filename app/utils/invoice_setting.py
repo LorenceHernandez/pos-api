ae8d910779a5f3b313e8791c21e4c555f841fb36
Christian <pujado.chris@gmail.com>
2024-08-20 22:51:16 +0800

    Update invoice setting prefix

--- app/utils/invoice_setting.py
+++ app/utils/invoice_setting.py
@@ -9,4 +9,4 @@ from app.utils.utils import getTimeZone
 def generate_invoice_str(prefix, postfix):
     date = str(datetime.now(getTimeZone()).date())
     new_date_string = date.replace("-", "")
-    return f'INV-{prefix}{new_date_string}{postfix}'
+    return f'N{prefix}{new_date_string}{postfix}'
