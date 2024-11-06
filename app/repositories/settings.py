805ed015565cabfcdbe2ab6ccc50771dbf381b71
Christian <pujado.chris@gmail.com>
2024-11-06 11:01:48 +0800

    Fix imports

--- app/repositories/settings.py
+++ app/repositories/settings.py
@@ -2,8 +2,6 @@
 
 
 from app.repositories.base import BackupRepository
-from app.utils.generate_invoice import generate_invoice_str
-from app.database.config import counters
 
 
 class SettingRepository(BackupRepository):
