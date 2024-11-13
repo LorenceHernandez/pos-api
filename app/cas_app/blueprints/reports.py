24bb56e715dd9689d60504f4c4624385757b8554
Lorence <lorencehernandez97@gmail.com>
2024-11-13 23:42:10 +0800

    test

--- app/cas_app/blueprints/reports.py
+++ app/cas_app/blueprints/reports.py
@@ -61,6 +61,7 @@ def get_trial_balance(user_id):
             accounting = data['accounting']
             totalAmountPaid = data['totalAmountPaid']
             for accounting_item in accounting:
+                print(accounting_item)
                 code_credit = accounting_item[account_code_credit]
                 code_debit = accounting_item[account_code_debit]
                 for chart_of_account_data in ret:
