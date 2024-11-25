a0655a9e88d64b9b800ffd3fdd0ee906bfeed628
Christian <pujado.chris@gmail.com>
2024-11-25 20:30:16 +0800

    Lookup acc types & groups in coa api

--- app/repositories/chart_account.py
+++ app/repositories/chart_account.py
@@ -14,12 +14,55 @@ class ChartAccountRepository(Repository):
                 { '$match': query },
                 {
                     '$addFields': {
+                        'accountGroup': {'$toObjectId': '$accountGroup'},
+                        'accountType': {'$toObjectId': '$accountType'}
+                    }
+                },
+                { 
+                    '$lookup': {
+                        'from': 'accountstype',
+                        'localField': 'accountType',
+                        'foreignField': '_id',
+                        'as': 'accountType'
+                    }, 
+                },
+                { 
+                    '$lookup': {
+                        'from': 'accountsgroup',
+                        'localField': 'accountGroup',
+                        'foreignField': '_id',
+                        'as': 'accountGroup'
+                    }, 
+                },
+                { "$unwind": {
+                    'path': "$accountType",
+                    'preserveNullAndEmptyArrays': True    
+                }},
+                { "$unwind": {
+                    'path': "$accountGroup",
+                    'preserveNullAndEmptyArrays': True    
+                }},
+                {
+                    '$addFields': {
                         '_id': {'$toString': '$_id' },
+                        'accountGroup._id': {'$toString': '$accountGroup._id' },
+                        'accountType._id': {'$toString': '$accountType._id' },
                     }
                 },
                 { '$sort': {"_id":-1} },
                 *args,
             ]))
-            return data
+
+            coa = []
+            for item in data:
+                if item.get('accountGroup') is None or item.get('accountGroup').get('_id') is None:
+                    item['accountGroup'] = None
+                
+                if item.get('accountType') is None or item.get('accountType').get('_id') is None:
+                    item['accountType'] = None
+                
+                coa.append(item)
+            return coa
+
         except Exception as e:
             raise e
