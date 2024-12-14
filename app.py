b6e0198374b4b2e015719cc2431dd1a6c4c6430d
Lorence Hernandez <lorencehernandez97@gmail.com>
2024-12-14 11:57:21 +0800

    test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from app.blueprints.audit_log import audit_log_bp
 from app.blueprints.reports import discount_reports_bp
 from app.blueprints.cashier_report import cashier_report_bp
 from app.blueprints.branch_report import branch_report_bp
-# from app.blueprints.sales import sales_bp
+from app.blueprints.sales import sales_bp
 # from app.blueprints.transaction import transaction_bp
 # from app.cas_app.blueprints.accounts_group import accounts_group_bp
 # from app.cas_app.blueprints.accounts_type import accounts_type_bp
