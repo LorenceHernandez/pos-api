795a40ee822e3c8f2880abe8113a6e8f0336c7f6
Lorence Hernandez <lorencehernandez97@gmail.com>
2024-12-14 11:56:45 +0800

    test

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from flask_cors import CORS, cross_origin
 from app.blueprints.audit_log import audit_log_bp
 from app.blueprints.reports import discount_reports_bp
 from app.blueprints.cashier_report import cashier_report_bp
-from app.blueprints.branch_report import branch_report_bp
-from app.blueprints.sales import sales_bp
-from app.blueprints.transaction import transaction_bp
-from app.cas_app.blueprints.accounts_group import accounts_group_bp
+# from app.blueprints.branch_report import branch_report_bp
+# from app.blueprints.sales import sales_bp
+# from app.blueprints.transaction import transaction_bp
+# from app.cas_app.blueprints.accounts_group import accounts_group_bp
 # from app.cas_app.blueprints.accounts_type import accounts_type_bp
 # from app.cas_app.blueprints.category import category_bp
 # from app.cas_app.blueprints.chart_account import chart_of_accounts_bp
